a008659ac2568bea354b8281877894617d291439
blackgauss <81921810+blackgauss@users.noreply.github.com>
2023-12-30 23:29:48 -0800

    prompt engineering

--- ContentGeneration/videoscript.py
+++ ContentGeneration/videoscript.py
@@ -64,23 +64,65 @@ def chat_gpt(prompt, model_name="gpt-3.5-turbo"):
     )
     return response.choices[0].message.content.strip()
 
+def rank_interest(reference):
+    with open('../data/gpt_prompts/topic_chooser.txt', 'r') as file:
+        interest_prompt = file.read()
+    prompt = make_prompt(interest_prompt, reference)
+    interest = chat_gpt(prompt, model_name='gpt-3.5-turbo')
+    try:
+        return int(interest)
+    except:
+        return None
 
-def generate_script(prompt, reference_path, prompt_path, model_name="gpt-3.5-turbo"):
-    script = chat_gpt(prompt, model_name)
-    return script
-
-def create_script(post, reference):
+    return interest
+def create_draft(reference):
     with open('../data/gpt_prompts/script_writer.txt', 'r') as file:
         writer_prompt = file.read()
     with open('../data/gpt_prompts/hook_editor.txt', 'r') as file:
         hook_prompt = file.read()
     with open('../data/gpt_prompts/script_editor.txt', 'r') as file:
         editor_prompt = file.read()
-    reference = format_script(post)
     first_prompt = make_prompt(writer_prompt, reference)
     script = chat_gpt(first_prompt, model_name='gpt-4-1106-preview')
-    second_prompt = make_prompt(hook_prompt, script)
-    edited_script = chat_gpt(second_prompt, model_name='gpt-4-1106-preview')
-    third_prompt = make_prompt(editor_prompt, edited_script)
-    edited_script2 = chat_gpt(third_prompt, model_name='gpt-4-1106-preview')
-    return edited_script2
+    return script
+
+def improve_hook(script):
+    with open('../data/gpt_prompts/hook_editor.txt', 'r') as file:
+        hook_prompt = file.read()
+    prompt = make_prompt(hook_prompt, script)
+    edited_hook = chat_gpt(prompt, model_name='gpt-4-1106-preview')
+    return edited_hook
+
+
+def edit_script(script):
+    with open('../data/gpt_prompts/script_editor.txt', 'r') as file:
+        editor_prompt = file.read()
+    prompt = make_prompt(editor_prompt, script)
+    edited_script = chat_gpt(prompt, model_name='gpt-4-1106-preview')
+    return edited_script
+
+
+def critique_script(script):
+    with open('../data/gpt_prompts/viewer_critique.txt', 'r') as file:
+        critique_prompt = file.read()
+    prompt = make_prompt(critique_prompt, script)
+    critique = chat_gpt(prompt, model_name='gpt-4-1106-preview')
+    return critique
+
+
+def revise_script(script, critique):
+    with open('../data/gpt_prompts/script_reviser.txt', 'r') as file:
+        revision_prompt = file.read()
+    reference = f'Criticism:{critique}\n Script:{script}'
+    prompt = make_prompt(revision_prompt, reference)
+    revision = chat_gpt(prompt, model_name='gpt-4-1106-preview')
+    return revision
+
+
+def score_script(script):
+    with open('../data/gpt_prompts/script_scorer.txt', 'r') as file:
+        scoring_prompt = file.read()
+    prompt = make_prompt(scoring_prompt, script)
+    score = chat_gpt(prompt, model_name='gpt-4-1106-preview')
+    return score
+    
